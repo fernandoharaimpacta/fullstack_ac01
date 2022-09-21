d3194d986c5d2cd0546a1fb4af48954ccf8ce146
fernandoharaimpacta <fernando.hara@aluno.faculdadeimpacta.com.br>
2022-09-20 21:59:36 -0300

    Correções

---
+++ fullstack_ac01/src/pages/Contact/index.js
@@ -0,0 +1,13 @@
+import Header from "../../components/Header";
+
+function Contact() {
+    return (
+        <div>
+            <Header />
+
+            <h1>Você está na Contact</h1>
+        </div>
+    );
+}
+
+export default Contact;

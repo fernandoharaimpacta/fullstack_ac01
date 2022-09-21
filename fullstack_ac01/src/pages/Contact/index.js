0d09c0a10b3a9e19ce9db3029ecbb4e8c1c6b6b8
fernandoharaimpacta <fernando.hara@aluno.faculdadeimpacta.com.br>
2022-09-20 22:20:32 -0300

    inclusão atividade

--- fullstack_ac01/src/pages/Contact/index.js
+++ fullstack_ac01/src/pages/Contact/index.js
@@ -7,6 +7,12 @@ function Contact() {
         email: "",
         message: ""        
     }) 
+
+    function submit(e) {
+        e.preventDefault();
+        alert(data.name + ' obrigado.');
+    }
+
     function handle(e) {
         const newdata = {...data}
         newdata[e.target.id] = e.target.value
@@ -18,13 +24,14 @@ function Contact() {
             <Header />
 
             <h1>Você está na Contact</h1>
-            <form>
+            <form onSubmit={(e) => submit(e)}>
                 Nome:<br/>
                 <input onChange={(e) => handle(e)} id="name" value={data.name} placeholder="name" type="text"></input><br/>
                 E-mail:<br/>
                 <input onChange={(e) => handle(e)} id="email" value={data.email} placeholder="e-mail" type="text"></input><br/>
                 Message:<br/>
-                <input onChange={(e) => handle(e)} id="message" value={data.message} placeholder="message" type="text"></input>
+                <input onChange={(e) => handle(e)} id="message" value={data.message} placeholder="message" type="text"></input><br/>
+                <button>Send</button>
             </form>
         </div>
     );

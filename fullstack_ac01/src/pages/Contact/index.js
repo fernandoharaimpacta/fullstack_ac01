e9605a5b492101966597e4e96fc54a0539d83074
fernandoharaimpacta <fernando.hara@aluno.faculdadeimpacta.com.br>
2022-09-20 22:15:19 -0300

    Alteração contact

--- fullstack_ac01/src/pages/Contact/index.js
+++ fullstack_ac01/src/pages/Contact/index.js
@@ -1,11 +1,31 @@
+import { useState } from "react";
 import Header from "../../components/Header";
 
 function Contact() {
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        message: ""        
+    }) 
+    function handle(e) {
+        const newdata = {...data}
+        newdata[e.target.id] = e.target.value
+        setData(newdata)
+        console.log(newdata)
+    }
     return (
         <div>
             <Header />
 
             <h1>Você está na Contact</h1>
+            <form>
+                Nome:<br/>
+                <input onChange={(e) => handle(e)} id="name" value={data.name} placeholder="name" type="text"></input><br/>
+                E-mail:<br/>
+                <input onChange={(e) => handle(e)} id="email" value={data.email} placeholder="e-mail" type="text"></input><br/>
+                Message:<br/>
+                <input onChange={(e) => handle(e)} id="message" value={data.message} placeholder="message" type="text"></input>
+            </form>
         </div>
     );
 }
